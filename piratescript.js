f1494afba26b22166d588be1c426ced46f289111
HJ3099 <104030833+HJ3099@users.noreply.github.com>
2022-05-16 07:38:34 -0700

    done
    i did the irateparr thing

--- piratescript.js
+++ piratescript.js
@@ -1,6 +1,26 @@
 
 function piratize(input){
 
-    // add you code in here
+    const words = input.split (' '); 
+    const translatedwords = []
+    for (let element of words) {
+        console.log(element);
+        if(element==="pounds"){
+            translatedwords.push("doubloons")
+        }
+        else if(Number(element)){
+            translatedwords.push(element);
+        }
+        else{
+            let ArrWord = '';
+            let Phrase = element.substring(1);
+            ArrWord = Phrase + element[0] + "arr"
+            translatedwords.push(ArrWord);
+        }
+    }
+    let PirateCode = ' ';
+    PirateCode = translatedwords.join(' ');
+    return PirateCode; 
+
     
 } 
